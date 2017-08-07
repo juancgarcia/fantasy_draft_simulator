170cf800be0367a116f6ae892e5b6566abadb3a9
Charles Cox <cox.charles.w@gmail.com>
2017-08-07 16:11:34 -0400

    getting help

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,5 +1,4 @@
 class LeaguesController < ApplicationController
-  before_action :set_league, only: [:show, :edit, :update, :destroy]
 
   # GET /leagues
   # GET /leagues.json
@@ -10,6 +9,7 @@ class LeaguesController < ApplicationController
   # GET /leagues/1
   # GET /leagues/1.json
   def show
+    @league = League.find(params[:id])
   end
 
   # GET /leagues/new
@@ -25,7 +25,9 @@ class LeaguesController < ApplicationController
   # POST /leagues
   # POST /leagues.json
   def create
+    # @new_league = League.new_league
     @league = League.new(league_params)
+    @league.add_teams
     respond_to do |format|
       if @league.save
         format.html { redirect_to @league, notice: 'League was successfully created.' }
@@ -62,12 +64,6 @@ class LeaguesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_league
-      @league = League.find(params[:id])
-      
-    end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def league_params
       params.require(:league).permit(:name)
--- app/models/league.rb
+++ app/models/league.rb
@@ -1,4 +1,10 @@
 class League < ApplicationRecord
   has_many :teams, :dependent => :destroy
   accepts_nested_attributes_for :teams
+
+  def add_teams
+    12.times do |i|
+      league.teams.create!(name: "Team #{i}")
+    end
+  end
 end
